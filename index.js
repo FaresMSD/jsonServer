3f8a86dc00b59b28c5dd14b9179d1f764c0548d2
Fares <fares.msd@icloud.com>
2022-04-13 13:47:05 +0200

    not due to image

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = () => {
     const data = {users: [],companies:[]}
     for (let i = 0; i < 1000; i++) {
         (i % 200 == 0 && data.companies.push({ id:faker.datatype.uuid(), name:faker.company.companyName()}) )
-        data.users.push({ id: faker.datatype.uuid(), name: faker.name.findName(), image: faker.phone.phoneNumber(),companyId:[...data.companies].pop().id });
+        data.users.push({ id: faker.datatype.uuid(), name: faker.name.findName(), image: faker.image.avatar(),companyId:[...data.companies].pop().id });
     }
 
 
